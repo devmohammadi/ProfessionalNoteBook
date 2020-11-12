fb366d848f723f2cfbd9d976aab51674840e2ffe
Fatemeh Mohammadi <79f.mohammadi@gmail.com>
2020-11-12 19:28:46 +0330

    Initial commit

---
+++ app/src/main/java/com/mohammadi/dashti/professionalnotebook/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mohammadi.dashti.professionalnotebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
